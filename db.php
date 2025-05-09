bdbab36dc2dee55c0d21bf32d0a98e2ad3fde1af
nitish421990 <41812918+nitish421990@users.noreply.github.com>
2025-05-10 00:23:58 +0530

    php crud application db connection
    php crud application db connection

---
+++ db.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+database : php-crud
+table : users
+table column: id, name, email,mobile, city, updated_at, created_at
+
+How to Create table by MySql query
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mobile` varchar(15) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+);
+*/ 
+
+$localhost="localhost";
+$username="root";
+$password="";
+$db="php-crud";
+
+$connect =new mysqli($localhost,$username,$password,$db);
+
+if($connect){
+    echo "successfully connected";
+}else{
+    echo "not connected";
+}
+
+
+?>
